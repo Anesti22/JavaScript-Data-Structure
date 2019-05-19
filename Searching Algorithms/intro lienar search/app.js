e2dd4f036397a3d3e12f4eb1d3e18f65b17a5c59
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2019-05-19 10:07:49 +0200

    Linear Search Solution

--- Searching Algorithms/intro lienar search/app.js
+++ Searching Algorithms/intro lienar search/app.js
@@ -1,9 +1,17 @@
 /* Searching Algorithms */
 //
 var usernames = ["anesti", "maria", "toto", "lula", "Gideon", "vita", "sokrati", "julisa", "driona", "ziniu", "xhina", "goni", "cezi", "flori", ];
-usernames.indexOf('toto');
 
-//
-var states = ["Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District Of Columbia", "Federated States Of Micronesia", "Florida", "Georgia", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Marshall Islands", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Northern Mariana Islands", "Ohio", "Oklahoma", "Oregon", "Palau", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virgin Islands", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
-]
+function linearSearch(arr, val) {
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i] === val) return i;
+    }
+    return -1;
+}
 
+linearSearch([32, 43, 56, 2], 2);
+
+// Write a function that accepts an array and a value.
+// Loop through the array and check if the current array element is equal to the value
+// If it is, return index at which the elemnt is found
+// If the value is never found, return -1

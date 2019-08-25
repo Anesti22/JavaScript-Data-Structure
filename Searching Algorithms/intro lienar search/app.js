b4510bb4f8e138a3ab7b89012f17b33392c65502
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2019-08-25 18:05:18 +0200

    Linear Search

--- Searching Algorithms/intro lienar search/app.js
+++ Searching Algorithms/intro lienar search/app.js
@@ -3,15 +3,16 @@
 var usernames = ["anesti", "maria", "toto", "lula", "Gideon", "vita", "sokrati", "julisa", "driona", "ziniu", "xhina", "goni", "cezi", "flori", ];
 
 function linearSearch(arr, val) {
-    for(var i = 0; i < arr.length; i++) {
+    for(let i=0; i = arr.lenght; i++) {
         if(arr[i] === val) return i;
     }
-    return -1;
-}
 
-linearSearch([32, 43, 56, 2], 2);
+    return -1
+}
+// linearSearch([32, 43, 56, 2], 2);
 
 // Write a function that accepts an array and a value.
 // Loop through the array and check if the current array element is equal to the value
 // If it is, return index at which the elemnt is found
 // If the value is never found, return -1
+// linearSearch([32, 43, 56, 2], 2);
